96e8ba12d1a22ea6c8c9635b473cf84af3ac43f5
Bryan <Bryan@128.4.59.159>
2016-09-25 19:47:03 -0400

    First commit

---
+++ TuitionCalculator/src/TuitonCalc.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class TuitonCalc {
+
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		double initialCost = 0;
+		double percentInc = 0;
+		double totalCost = 0;
+		
+		System.out.println("Initial tuition cost?");
+		initialCost = scan.nextDouble();
+
+		System.out.println("Percentage increase in tuition?");
+		percentInc = scan.nextDouble()/100; 
+		
+		totalCost = initialCost; 
+
+		double cost = initialCost; 
+		
+		for (int i = 0; i < 3; i++) {
+			cost += percentInc*cost;
+			totalCost += cost;
+		}
+		
+		System.out.println("Cost of tuition:");		
+		System.out.println(totalCost);
+		
+	}
+
+}
